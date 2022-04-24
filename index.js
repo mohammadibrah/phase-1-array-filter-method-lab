5f66400ae2f847fa9af93511ace27aa63b38d318
m.ibrahim <mohammad.si.tt@gmail.com>
2022-04-25 01:12:37 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(arrOfDrivers, name){
+    return arrOfDrivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(arrOfDrivers, name){
+    return arrOfDrivers.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()))
+}
+// Referance of startsWith()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+
+function matchName (arrOfDriversObs, name){
+    return arrOfDriversObs.filter(driver => driver.name.toLowerCase() === name.toLowerCase())
+}
